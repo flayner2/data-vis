596d270b2ec75ebf26b7a1f5a871bc569e74e8f6
flayner2 <flayner5@gmail.com>
2021-06-28 20:05:09 -0300

    Added a script to hold some global variables

---
+++ src/globalVars.js
@@ -0,0 +1,10 @@
+const globalColors = {
+  white: '#dfbf8e',
+  green: '#a9b665',
+  red: '#ea6962',
+  light_orange: '#e3a84e',
+  black: '#665c54',
+  bg: '#282828',
+  blue: '#7daea3',
+  pink: '#d3869b',
+};
