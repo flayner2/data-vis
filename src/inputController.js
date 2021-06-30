4b71264eb00a448bc524e23e76ea470ca14ebef5
flayner2 <flayner5@gmail.com>
2021-06-30 10:50:44 -0300

    Added a way to update the charts on input value change

--- src/inputController.js
+++ src/inputController.js
@@ -1,22 +1,33 @@
 const walkerVarInputs = document.querySelectorAll('.walker-variables');
 
 for (let walkerInput of walkerVarInputs) {
-  console.log(walkerInput);
   walkerInput.oninput = function () {
     // Doesn't let the user type a non-numeric character
     walkerInput.value = walkerInput.value.replace(/[^0-9]/g, '');
 
-    let id = walkerInput.attributes.name.value;
+    const id = walkerInput.attributes.name.value;
     updateChart(id);
   };
 }
 
 function updateChart(id) {
-  let rootId = id.split('-').splice(0, 2).join('-');
+  const rootId = id.split('-').splice(0, 2).join('-');
+  const newN = Number(
+    document.querySelector(`input.walker-variables[name='${rootId}-n']`).value
+  );
+  const newR = Number(
+    document.querySelector(`input.walker-variables[name='${rootId}-r']`).value
+  );
 
   switch (rootId) {
     case 'chart-1':
-      generateChart1();
+      const { walker, steps } = defaultRandomWalk(newN, newR);
+      const labels = [...Array(newN + 1).keys()];
+      chart1.data.labels = labels;
+      chart1.data.datasets.forEach((dataset) => {
+        dataset.data = steps;
+      });
+      chart1.update();
       break;
     case 'chart-2':
       generateChart2();
