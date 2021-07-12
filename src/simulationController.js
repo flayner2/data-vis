54ff70813e5a51efb1b7752ffba734bbacba2a1a
flayner2 <flayner5@gmail.com>
2021-07-12 10:51:20 -0300

    Added the first static simulation

--- src/simulationController.js
+++ src/simulationController.js
@@ -5,6 +5,57 @@ function getCanvasSize(canvas) {
   return {"width": canvas.clientWidth, "height": canvas.clientHeight}
 }
 
+function checkCorners(p, walker) {
+  if (walker.x > p.width) {
+    walker.x = 0;
+  } else if (walker.x < 0) {
+    walker.x = p.width;
+  }
+
+  if (walker.y > p.height) {
+    walker.y = 0;
+  } else if (walker.y < 0) {
+    walker.y = p.height;
+  }
+}
+
+const simulation1 = (p) => {
+  let {width: canvasWidth, height: canvasHeight} = getCanvasSize(canvas2);
+  let walker;
+  let h, s, b;
+
+  p.setup = function() {
+    p.createCanvas(canvasWidth, canvasHeight);
+    p.background(globalColors.black);
+
+    p.colorMode('HSB', 360, 100, 100);
+    p.strokeWeight(2);
+
+    walker = p.createVector(p.width / 2, p.height / 2);
+
+    for (let i = 0; i < 10000; i++){
+      let stepY = p.random([5, -5]);
+      let stepX = p.random([5, -5]);
+
+      h = p.map(walker.y, 0, p.height, 0, 360);
+      s = 50;
+      b = 50;
+
+      p.stroke(h, s, b);
+      p.line(walker.x, walker.y, walker.x + stepX, walker.y + stepY);
+
+      walker.x += stepX;
+      walker.y += stepY;
+
+      checkCorners(p, walker); 
+    }
+  }
+
+  p.windowResized = function () {
+    p.resizeCanvas(canvas1.clientWidth, canvas1.clientHeight);
+  };
+}
+
 const simulation2 = (p) => {
   let {width: canvasWidth, height: canvasHeight} = getCanvasSize(canvas2);
   let walker;
@@ -36,17 +87,7 @@ const simulation2 = (p) => {
     walker.x += stepX;
     walker.y += stepY;
 
-    if (walker.x > p.width) {
-      walker.x = 0;
-    } else if (walker.x < 0) {
-      walker.x = p.width;
-    }
-
-    if (walker.y > p.height) {
-      walker.y = 0;
-    } else if (walker.y < 0) {
-      walker.y = p.height;
-    }
+    checkCorners(p, walker);
   };
 
   p.windowResized = function () {
@@ -54,4 +95,5 @@ const simulation2 = (p) => {
   };
 };
 
+new p5(simulation1, canvas1);
 new p5(simulation2, canvas2);
