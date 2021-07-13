4e23ed2baa8cc538eb33c52da0521373e24eff11
flayner2 <flayner5@gmail.com>
2021-07-12 21:42:34 -0300

    Removed an unecessary file

--- src/inputController.js
+++
@@ -1,8 +0,0 @@
-const walkerVarInputs = document.querySelectorAll('.walker-variables');
-
-for (let walkerInput of walkerVarInputs) {
-  walkerInput.oninput = function () {
-    // Doesn't let the user type a non-numeric character
-    walkerInput.value = walkerInput.value.replace(/[^0-9]/g, '');
-  };
-}
